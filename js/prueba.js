e425f9c3dcedef9884596b325bae2c0b77e18f42
Jguerrero45 <guerrerojosegarcia@gmail.com>
2024-11-03 18:49:48 -0400

    cambiando porcentajes

--- js/prueba.js
+++ js/prueba.js
@@ -19,7 +19,7 @@ let endState = 100; // Tickets Totales %
 progressBarStates.forEach(state => {
   let randomTime = Math.floor(Math.random() * 50);
   setTimeout(() => {
-    if(state == endState){
+    if (state == endState) {
       gsap.to(progressBar, {
         x: `${state}%`,
         duration: 2,
@@ -29,7 +29,7 @@ progressBarStates.forEach(state => {
           progressBarContainer.style.boxShadow = '0 0 5px #4895ef';
         }
       });
-    }else{
+    } else {
       gsap.to(progressBar, {
         x: `${state}%`,
         duration: 2,
