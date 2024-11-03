d9da870c3fcd766530052bc2bf1446751f2beedc
abelipe <157862231+abelipe@users.noreply.github.com>
2024-11-03 16:56:15 -0400

    adding buttons matrix

--- js/archivo.js
+++ js/archivo.js
@@ -1,3 +1,31 @@
+
+
+const x = 100; // Tickets Totales
+const ArrayBoletosNoDisponible = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const ArrayBoletosDisponible = Array.from({ length: x + 1 }, (_, i) => i);
+
+function botonComprarPorNumero() {
+    const container = document.querySelector('.days-btn-container');
+    ArrayBoletosDisponible.forEach(boleto => {
+        if (ArrayBoletosNoDisponible.includes(boleto)) {
+            return; // Skip this boleto if it's in ArrayBoletosNoDisponible
+        }
+
+        const input = document.createElement('input');
+        input.className = 'day-btn';
+        input.id = boleto;
+        input.type = 'checkbox';
+        input.checked = false;
+
+        const label = document.createElement('label');
+        label.className = 'day-label';
+        label.htmlFor = boleto;
+        label.textContent = boleto;
+
+        container.appendChild(input);
+        container.appendChild(label);
+    });
+}
 function enviarWhatsApp() {
     var cedula = document.getElementById('cedula').innerText;
     var nombre = document.getElementById('nombre').innerText;
