f48be9535f49e3448153851839f7afc1deed42e1
abelipe <157862231+abelipe@users.noreply.github.com>
2024-11-08 00:06:35 -0400

    adding logic to check boxes and luck based button

--- public/js/archivo.js
+++ public/js/archivo.js
@@ -4,18 +4,43 @@ const x = 1000; // Tickets Totales
 const ArrayBoletosNoDisponible = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const ArrayBoletosDisponible = Array.from({ length: x + 1 }, (_, i) => i.toString().padStart(4, '0'));
 
+let selectedCount = 0;
+let selectedBoletos = [];
+
+selecting = false;
+
+function updateSelection(boleto, isChecked) {
+    if (isChecked) {
+        selectedCount += 1;
+        selectedBoletos.push(boleto);
+    } else {
+        selectedCount -= 1;
+        selectedBoletos = selectedBoletos.filter(item => item !== boleto);
+    }
+    console.log(`Selected Count: ${selectedCount}`);
+    console.log(`Selected Boletos: ${selectedBoletos}`);
+}
+
+
+
 function botonComprarPorNumero() {
     const container = document.querySelector('.days-btn-container');
+    container.innerHTML = `
+        
+    `;
     ArrayBoletosDisponible.forEach(boleto => {
         if (ArrayBoletosNoDisponible.includes(parseInt(boleto))) {
             return; // Skip this boleto if it's in ArrayBoletosNoDisponible
         }
-
         const input = document.createElement('input');
         input.className = 'day-btn';
         input.id = boleto;
         input.type = 'checkbox';
         input.checked = false;
+
+        input.addEventListener('change', (event) => {
+            updateSelection(boleto, event.target.checked);
+        });
 
         const label = document.createElement('label');
         label.className = 'day-label';
@@ -27,14 +52,97 @@ function botonComprarPorNumero() {
     });
     const button = document.getElementById('Numero');
     button.disabled = true;
+    const button2 = document.getElementById('Suerte');
+    button2.disabled = false;
+    selecting = true;
 }
+
+function botonComprarPorSuerte() {
+    const container = document.querySelector('.days-btn-container');
+    container.innerHTML = `
+    <div class="button-container">
+        <p>Los números seran seleccionados al azar, de la cantidad tickets que desea.</p>
+    </div>
+        <div class="button-container">
+            <button class="button-3d" id="minus" onclick="minus()">
+                <div class="button-top">
+                    <span class="material-icons">-</span>
+                </div>
+                <div class="button-bottom"></div>
+                <div class="button-base"></div>
+            </button>
+            <label for="numeros" class="numeros" id="numeros">0</label>
+            <button class="button-3d" id="plus" onclick="plus()">
+                <div class="button-top">
+                    <span class="material-icons">+</span>
+                </div>
+                <div class="button-bottom"></div>
+                <div class="button-base"></div>
+            </button>
+        </div>
+    `;
+
+    const button = document.getElementById('Suerte');
+    button.disabled = true;
+    const button2 = document.getElementById('Numero');
+    button2.disabled = false;
+    selecting = false;
+    selectedCount = 0;
+    selectedBoletos = [];
+    
+
+}
+
+function plus() {
+    const label = document.getElementById('numeros');
+    let currentValue = parseInt(label.textContent);
+    label.textContent = currentValue + 2;
+}
+
+function minus() {
+    const label = document.getElementById('numeros');
+    let currentValue = parseInt(label.textContent);
+    if (currentValue > 0) {
+        label.textContent = currentValue - 2;
+    }
+    
+}
+
 function enviarWhatsApp() {
     var cedula = document.getElementById('cedula').value;
     var nombre = document.getElementById('nombre').value;
     var telefono = document.getElementById('telefono').value;
     var estado = document.getElementById('estado').value;
+    var boletos = 0;
+    var NmrosBoletos = '';
+    if (selecting) {
+        if (selectedCount === 0 || selectedCount % 2 !== 0) {
+            alert('Debe seleccionar minimo de 2 numeros y unicamente en numeros pares (2, 4, 6, 8, 10, etc.)');
+            return;
+        }
+        boletos = selectedCount;
+        NmrosBoletos = selectedBoletos.join(', ');
+    } else if (!selecting) {
+        boletos = document.getElementById('numeros').textContent;
+        var Rnumeros = parseInt(boletos);
+        if (Rnumeros === 0) {
+            alert('Debe seleccionar minimo de 2 numeros y unicamente en numeros pares (2, 4, 6, 8, 10, etc.)');
+            return;
+        }else{
+            for (let i = 0; i < Rnumeros; i++) {
+                var numero = Math.floor(Math.random() * 1000);
+                if (ArrayBoletosNoDisponible.includes(numero)) {
+                    i--;
+                } else {
+                    NmrosBoletos += numero + ', ';
+                }
+            }
+            NmrosBoletos = NmrosBoletos.substring(0, NmrosBoletos.length - 2);
+        }
+    }
 
-    var mensaje = `Cédula: ${cedula}\nNombre: ${nombre}\nTeléfono: ${telefono}\nEstado: ${estado}`;
+    
+    var mensaje = `Cédula: ${cedula}\nNombre: ${nombre}\nTeléfono: ${telefono}\nEstado: ${estado}\nCantidad de Boletos: ${boletos}\nNmros de Boletos: ${NmrosBoletos}`;
     var mensajeCodificado = encodeURIComponent(mensaje);
     var numeroTelefono = '584124007847';
     var url = `https://wa.me/${numeroTelefono}?text=${mensajeCodificado}`;
