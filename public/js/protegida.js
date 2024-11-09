1727054521a26da3800f2f6bb15d434b3ffe8034
Jguerrero45 <guerrerojosegarcia@gmail.com>
2024-11-08 21:31:09 -0400

    ahora la ruta protegida tiene una alerta por si se ingreso un numero mayor a 4 digitos

--- public/js/protegida.js
+++ public/js/protegida.js
@@ -1,27 +1,29 @@
 function revision() {
     var numeroBoleto = document.getElementById('id_boleto').value;
-    var pago = document.getElementById('pago').value; // Get the selected payment status
+    var pago = document.getElementById('pago').value; // Obtener el estado de pago seleccionado
 
     if (numeroBoleto.length < 4) {
         alert('El número de boleto debe tener 4 dígitos');
+    } else if (numeroBoleto.length > 4) {
+        alert('El número de boleto no debe tener más de 4 dígitos');
     } else {
         fetch('/save-boleto', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ 
+            body: JSON.stringify({
                 numeroBoleto: numeroBoleto.substring(0, 4),
-                pago: pago // Include the payment status in the request body
+                pago: pago // Incluir el estado de pago en el cuerpo de la solicitud
             })
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error al procesar el boleto');
-            }
-            return response.text();
-        })
-        .then(data => alert(data))
-        .catch(error => alert(error.message));
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al procesar el boleto');
+                }
+                return response.text();
+            })
+            .then(data => alert(data))
+            .catch(error => alert(error.message));
     }
 }
